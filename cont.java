b0f5d9a68cb811ec1dad85723e825a8ef452ebc6
Sairaj devadiga <sairajsherigar1@gmail.com>
2025-04-11 09:41:22 +0530

    Add files via upload

---
+++ cont.java
@@ -0,0 +1,14 @@
+public class cont {
+    public static void main(String[] args) {
+        int n=1234;
+        int rem=0;
+        int count=0;
+        while(n>0)
+        {
+            rem=n%10;
+            count++;
+            n=n/10;
+        }
+        System.out.println(count);
+    }
+}
