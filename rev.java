29f8ce35939cb0b69f1e42395b95b54caf914e28
Sairaj devadiga <sairajsherigar1@gmail.com>
2025-04-11 09:40:41 +0530

    Add files via upload

---
+++ rev.java
@@ -0,0 +1,12 @@
+public class rev {
+    public static void main(String[] args) {
+        int n=123456;
+        int rem=0;
+        while(n>0)
+        {
+            rem=n%10;
+            System.out.print(rem);
+            n=n/10;
+        }
+    }
+}
